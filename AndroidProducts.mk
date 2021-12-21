51e1482becbc10acde0a5ea7f20c36aa93c4a0be
Krxyzn <RafaIbrahim1211@gmail.com>
2021-12-21 12:22:05 +0000

    Santoni: BringUp Ancient
    Signed-off-by: Krxyzn <RafaIbrahim1211@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,35 +1,8 @@
-# ArrowOS
+# AncientOS
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_santoni.mk
+    $(LOCAL_DIR)/ancient_santoni.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_santoni-userdebug \
-    arrow_santoni-user \
-    arrow_santoni-eng
-
-# AEX
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/aosp_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    aosp_santoni-userdebug \
-    aosp_santoni-user \
-    aosp_santoni-eng
-
-# Octavi-OS
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/octavi_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    octavi_santoni-userdebug \
-    octavi_santoni-user \
-    octavi_santoni-eng
-
-# Syberia-Project
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/syberia_santoni.mk
-
-COMMON_LUNCH_CHOICES += \
-    syberia_santoni-userdebug \
-    syberia_santoni-user \
-    syberia_santoni-eng
+    ancient_santoni-userdebug \
+    ancient_santoni-user \
+    ancient_santoni-eng
